482b17330934e43a599ccb7fc286a424407490c8
dangtiendat24 <148854633+dangtiendat24@users.noreply.github.com>
2024-06-12 22:17:03 +0700

    Add files via upload

---
+++ Ex11_OOP/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace Ex11_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            try
+            {
+                Console.Write("Nhập tên file: ");
+                string fileName = Console.ReadLine();
+
+                Console.Write("Nhập nội dung file: ");
+                string content = Console.ReadLine();
+
+                File.WriteAllText(fileName, content, Encoding.UTF8);
+                Console.WriteLine("Nội dung đã được ghi vào file");
+
+                string readContent = File.ReadAllText(fileName, Encoding.UTF8);
+                Console.WriteLine("Nội dung đọc từ file là: ");
+                Console.WriteLine(readContent);
+            }catch(Exception ex)
+            {
+                Console.WriteLine("Đã xảy ra lỗi " +  ex.Message);
+            }
+        }
+    }
+}
